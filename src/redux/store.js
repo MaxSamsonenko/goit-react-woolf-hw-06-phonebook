85c8f4aa3fdaf85c4ebf43e1e8e37a7b63f7f471
Max Samsonenko <samsonenko.maxwork@gmail.com>
2024-03-12 15:37:21 +0200

    redux persist add

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,39 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { contactsReducer } from './contactsSlice';
 import { filterReducer } from './filterSlice';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import { persistReducer, persistStore } from 'redux-persist';
+
+const rootReducer = combineReducers({
+  contacts: contactsReducer,
+  filter: filterReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    contacts: contactsReducer,
-    filter: filterReducer,
-  },
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        // Ignore these action types
+        ignoredActions: [
+          'persist/PERSIST',
+          'persist/REHYDRATE',
+          'persist/REGISTER',
+        ],
+
+        ignoredActionPaths: ['payload.register', 'payload.rehydrate'],
+
+        ignoredPaths: ['some.path.to.ignore'],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
